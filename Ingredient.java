cd694efd3a01e840121007032fc57ddd13a0c8ca
saidasaduakas <80287291+saidasaduakas@users.noreply.github.com>
2021-11-10 12:41:37 +0600

    Create Ingredient.java

---
+++ Ingredient.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Ingredient {
+    public String getPizzaitem(){
+        return "dough, pizza sause, Mozzarella Cheese";
+    }
+    public String getPastaitem(){
+        return "onion, flour, tomato";
+    }
+}
